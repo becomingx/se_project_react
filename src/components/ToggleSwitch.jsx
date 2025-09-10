9c5182c0aef2068bd2d7f3e649aa29d993fab26e
becomingx <patriciatirado9@gmail.com>
2025-09-10 18:50:37 -0400

    developing ToggleSwitch.jsx

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -3,5 +3,8 @@ import "../blocks/ToggleSwitch.css";
 export default function  ToggleSwitch() {
     return <label className="toggle-switch">
         <input type="checkbox" className="toggle-switch__checkbox" />
+        <span className="toggle-switch__circle">F</span>
+        <span class="toggle-switch__C">C</span>
     </label>
 }
+
